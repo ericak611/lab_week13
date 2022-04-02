58bacd058244cc3656fa398188f76664d6588a02
Hyewon Kim <hkim444@my.bcit.ca>
2022-04-01 23:05:38 -0700

    added JavaScript to app.js so that div in "originals" is copied to copies section when clicked

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-// const o_children = document.querySelector('.original-container').children;
-// const copy_cont = document.querySelector('.copy-container');
-// const btn = document.querySelector('button');
+const o_children = document.querySelector('.original-container').children;
+const copy_cont = document.querySelector('.copy-container');
+const btn = document.querySelector('button');
 
-// function copy() {
-//     for (let i=0; i < o_children.length; i++) {
-//         o_children[i].addEventListener('click', function (e) {
-//             const div = document.createElement('div');
+function copy() {
+    for (let i=0; i < o_children.length; i++) {
+        o_children[i].addEventListener('click', function (e) {
+            const div = document.createElement('div');
             
-//             const p = document.createElement('p');
-//             p.innerHTML = o_children[i].children[0].innerHTML;
+            const p = document.createElement('p');
+            p.innerHTML = o_children[i].children[0].innerHTML;
     
-//             div.appendChild(p);
-//             copy_cont.appendChild(div);
-//         });
-//     }
-// };
+            div.appendChild(p);
+            copy_cont.appendChild(div);
+        });
+    }
+};
 
 
 function clear() {
